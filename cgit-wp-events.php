8d8bb3ecafe63cb8f35fd140ef821b75a38cd7fb
AJReading <andy@castlegateit.co.uk>
2017-08-14 14:58:05 +0100

    Incremented version minor.

--- cgit-wp-events.php
+++ cgit-wp-events.php
@@ -5,7 +5,7 @@ Plugin Name: Castlegate IT WP Events
 Plugin URI: https://github.com/castlegateit/cgit-wp-events/
 Description: A simple and easy to use events interface with complete developer
 control.
-Version: 1.2
+Version: 1.2.1
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 */
