d2494d0a26014e56c7a1b3f93048179f000cce72
richplane <richard@castlegateit.co.uk>
2017-10-03 18:06:57 +0100

    (also need to change this)

--- user-guide.php
+++ user-guide.php
@@ -1,5 +1,3 @@
-<h3>Events</h3>
-
 <p>Events can be added and edited in the same way as posts or pages. You can view and edit all the events on the site using the &ldquo;Events&rdquo; menu on the left side of the admin panel. In addition to the default fields, events have fields for their time, date, and location.<?php
 
     $supports = array();
